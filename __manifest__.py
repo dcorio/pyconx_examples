e41f851d25a66d92739750355dbae66eeb3f4ac4
Davide Corio <enlightx@gmail.com>
2019-05-03 08:49:25 +0200

    [ADD] pyconx_examples

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'PyconX Examples',
+    'version': '12.0.1.0.0',
+    'category': 'Examples',
+    'summary': 'Esempi di personalizzazioni per PyconX',
+    'author': "Your Name Here",
+    'website': 'https://yourwebsite.here',
+    'license': 'AGPL-3',
+    'depends': ['website', 'website_sale'],
+    'data': [
+        'views/assets.xml',
+        'views/product_views.xml',
+        'views/templates.xml'],
+    'installable': True,
+}
